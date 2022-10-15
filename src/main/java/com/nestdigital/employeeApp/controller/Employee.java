8e0479d0750c13e1a5e930e7c05881c19c5a23f4
Liju-07 <lijusaju03@gmail.com>
2022-10-15 17:01:53 +0530

    api

--- src/main/java/com/nestdigital/employeeApp/controller/Employee.java
+++ src/main/java/com/nestdigital/employeeApp/controller/Employee.java
@@ -1,4 +1,31 @@
 package com.nestdigital.employeeApp.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
 public class Employee {
+    @GetMapping("")
+    public String employeeHome()
+    {
+        return "Employee Homepage";
+    }
+
+    @GetMapping("/add")
+    public String employeeAdd()
+    {
+        return "Employee Add";
+    }
+
+    @GetMapping("/delete")
+    public String employeeDelete()
+    {
+        return "Employee Delete";
+    }
+@GetMapping("/search")
+    public String employeeSearch()
+    {
+        return "Employee Search";
+    }
 }
